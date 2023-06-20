11607c974697980fb3b89c859d8bb9cf59ca78e7
kittipat19877 <number8671@gmail.com>
2023-06-20 12:07:26 +0700

    create react app

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+function Footer() {
+    return (
+        <div className='myfooter'>
+            <span>@Porsche</span>
+        </div>
+    );
+}
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+function Header() {
+    return (
+        <div className='myheader'>
+            <h1>react demo site</h1>
+        </div>
+    );
+}
+export default Header;
